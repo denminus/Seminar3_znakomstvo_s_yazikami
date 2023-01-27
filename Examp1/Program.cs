ed7ae25e69acc61b3b0a8a099001f4d799c052ae
den_minus <den.minus@mail.ru>
2023-01-27 13:00:27 +0300

    возведение числа а в натуральную степень В используя цикл, определение количества цифр в числе

---
+++ Examp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// возведите число а в натуральную степень В используя цикл
+/*Console.Write("Введите число А ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень В ");
+int B = int.Parse(Console.ReadLine()!);
+
+int power =1;
+for (int i=1 ; i<=B; i++)
+{
+    power*=A;  // A=A*power
+}
+Console.WriteLine("Число " +A+ " в степени " +B+ " равно " + power);
+
+*/
+// определение количества цифр в числе
+Console.Write("Введите число А ");
+int b = int.Parse(Console.ReadLine()!);
+int i=0;
+
+while (b>1)
+{
+    b=b/10;
+    i++;
+}
+
+Console.WriteLine("количество цифр " +i);
+
